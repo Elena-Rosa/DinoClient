f8b19a481fd88b1eb7e6d5639d7a3ec97da6965e
Elena <elenarosamail@gmail.com>
2023-11-13 13:49:00 -0800

    WIP

---
+++ CretaceousClient/Models/ApiHelper.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace CretaceousClient.Models
+{
+  public class ApiHelper
+  {
+    public static async Task<string> GetAll()
+    {
+      RestClient client = new RestClient("http://localhost:5000/");
+      RestRequest request = new RestRequest($"api/animals", Method.Get);
+      RestResponse response = await client.GetAsync(request);
+      return response.Content;
+    }
+    public static async Task<string> Get(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/");
+      RestRequest request = new RestRequest($"api/animals/{id}", Method.Get);
+      RestResponse response = await client.GetAsync(request);
+      return response.Content;
+    }
+  }
+}
